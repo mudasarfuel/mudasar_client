caf754b2b2b60b43df59f1b1bebc78171e68f1bf
Kashif Hussain <technologiessaimon@gmail.com>
2025-07-15 16:13:26 +0200

    Re organized customer slice as well

--- src/redux/customerSlice/customerSlice.js
+++ src/redux/customerSlice/customerSlice.js
@@ -3,257 +3,230 @@ import { ENDPOINTS } from "../../backend/API";
 import axios from "axios";
 import { toast } from "react-toastify";
 
-//GET ALL CUSTOMERS AXIOS CALL USING ASYNC THUNK
+// =============================================
+// ASYNC THUNK ACTIONS
+// =============================================
+
+/**
+ * Fetches customers with optional filtering, sorting and pagination
+ * @param {Object} initData - Contains query parameters
+ * @param {string} [initData.field] - Field to filter by
+ * @param {string} [initData.operator] - Comparison operator
+ * @param {number} initData.sort - Sort direction (-1/1)
+ * @param {number} initData.page - Page number
+ * @param {string} [initData.searchInput] - Search term
+ */
 export const getCustomers = createAsyncThunk(
   "getCustomers",
   async (initData) => {
     try {
       const { field, operator, sort, page, searchInput } = initData;
+      
+      // Debug logging
+      console.log("Field => ", field, "Operator =>", operator, 
+                 "sort => ", sort, "Page =>", page, 
+                 "Search Input =>", searchInput);
 
-      //Creating API call using ENDPOINTS as Base URL (/api/customers)
-      // field=${field}&operator=${operator}&searchInput=${searchInput}&
-      console.log(
-        "Field => ",
-        field,
-        "Operator =>",
-        operator,
-        "sort => ",
-        sort,
-        "Page =>",
-        page,
-        "Search Input =>",
-        searchInput
+      const response = await axios.get(
+        `${ENDPOINTS.CUSTOMER}?field=${field}&operator=${operator}&searchInput=${searchInput}&page=${page}&sort=${sort}`
       );
-      return await axios
-        .get(
-          `${ENDPOINTS.CUSTOMER}?field=${field}&operator=${operator}&searchInput=${searchInput}&page=${page}&sort=${sort}`
-        )
-        .then((res) => res.data);
+      return response.data;
     } catch (error) {
-      //Incase of error catch error
       return error.response.data.error[0];
     }
   }
 );
 
-//GET SINGLE CUSTOMER AXIOS CALL USING ASYNC THUNK
+/**
+ * Fetches a single customer by ID
+ * @param {string} id - Customer ID to fetch
+ */
 export const getSingleCustomer = createAsyncThunk(
   "getSingleCustomer",
   async (id) => {
     try {
-      //Creating API Call using base url (/api/customers/:id)
-      return await axios
-        .get(`${ENDPOINTS.CUSTOMER}/${id}`)
-        .then((res) => res.data);
+      const response = await axios.get(`${ENDPOINTS.CUSTOMER}/${id}`);
+      return response.data;
     } catch (error) {
-      //In case of error
       return error.response.data.error[0];
     }
   }
 );
-//ADD CUSTOMER AXIOS CALL USING ASYNC THUNK
+
+/**
+ * Creates a new customer
+ * @param {Object} Data - Customer data to create
+ */
 export const addCustomer = createAsyncThunk("addCustomer", async (Data) => {
   try {
-    //Creating API call using ENDPOINTS as Base URL (/api/webadmin/tenants)
-    return await axios.post(ENDPOINTS.CUSTOMER, Data).then((res) => res.data);
+    const response = await axios.post(ENDPOINTS.CUSTOMER, Data);
+    return response.data;
   } catch (error) {
-    //Incase of error catch error
     return error.response.data;
   }
 });
-//UPDATE CUSTOMER AXIOS CALL USING ASYNC THUNK
+
+/**
+ * Updates an existing customer
+ * @param {Object} initData - Contains ID and update data
+ * @param {string} initData.id - Customer ID to update
+ * @param {Object} initData.Data - Updated customer data
+ */
 export const updateCustomer = createAsyncThunk(
   "updateCustomer",
   async (initData) => {
     try {
-      //De Structuring data
       const { id, Data } = initData;
-
-      //Creating API call using ENDPOINTS as Base URL (/api/customers/:id)
-      return await axios
-        .put(`${ENDPOINTS.CUSTOMER}/${id}`, Data)
-        .then((res) => res.data);
+      const response = await axios.put(`${ENDPOINTS.CUSTOMER}/${id}`, Data);
+      return response.data;
     } catch (error) {
-      //Incase of error catch error
       return error.response.data;
     }
   }
 );
-//DELETE CUSTOMER AXIOS CALL USING ASYNC THUNK
+
+/**
+ * Deletes a customer by ID
+ * @param {string} id - Customer ID to delete
+ */
 export const deleteCustomer = createAsyncThunk("deleteCustomer", async (id) => {
   try {
-    //Creating API Call using ENDPOINTS as Base URL (/api/customers/:id)
-    return await axios
-      .delete(`${ENDPOINTS.CUSTOMER}/${id}`)
-      .then((res) => res.data);
+    const response = await axios.delete(`${ENDPOINTS.CUSTOMER}/${id}`);
+    return response.data;
   } catch (error) {
     return error.response.data.error[0];
   }
 });
-//Creating Customers Slice
+
+// =============================================
+// REDUX SLICE DEFINITION
+// =============================================
+
+const initialState = {
+  current: {},     // Currently selected customer
+  data: [],        // Array of customers
+  totalRecord: 0,  // Total count of records
+  errors: [],      // Error messages
+};
+
 export const customerSlice = createSlice({
   name: "customers",
-  initialState: {
-    current: {},
-    data: [],
-    totalRecord: 0,
-    errors: [],
-  },
+  initialState,
+  
+  // Synchronous reducers
   reducers: {
-    clearCustomers() {
-      return {
-        current: {},
-        data: [],
-        totalRecord: 0,
-        errors: [],
-      };
-    },
+    /**
+     * Resets customer state to initial values
+     */
+    clearCustomers: () => initialState
   },
+  
+  // Async action handlers
   extraReducers: (builder) => {
-    //@CaseNo       01
-    //@Request      GET
-    //@Status       Success
-    //@Loading      False
-    //@used For     GET CUSTOMERS
-    //@Data         Data stored in state
-    builder.addCase(getCustomers.fulfilled, (state, actions) => {
-      //Check for request success
-      if (actions.payload.success === true) {
-        //First Removing all the previous page customers
-        state.data = [];
-        //Using map iterate each item and push into the state
-        actions.payload.data.map((item) => {
-          //Here we are modifying the _id to id of each record
-          const customer = { ...item, id: item._id };
-          //Here we are setting the fetched customers in redux store
-          return state.data.push(customer);
-        });
-        //Here we are setting total number of records in redux store
-        state.totalRecord = actions.payload.totalRecords;
-      }
-    });
-
-    //@CaseNo       03
-    //@Request      GET
-    //@Status       Success
-    //@Loading      False
-    //@used For     GET SINGLE CUSTOMER
-    //@Response     Date Stored in State current
-    builder.addCase(getSingleCustomer.fulfilled, (state, action) => {
-      //Checking for success
+    
+    // =============================================
+    // GET CUSTOMERS HANDLER
+    // =============================================
+    builder.addCase(getCustomers.fulfilled, (state, action) => {
       if (action.payload.success === true) {
-        //Set state
-        return {
-          ...state,
-          current: action.payload.data,
-        };
+        // Clear existing data
+        state.data = [];
+        
+        // Transform and add new data
+        action.payload.data.forEach((item) => {
+          const customer = { ...item, id: item._id };
+          state.data.push(customer);
+        });
+        
+        // Update record count
+        state.totalRecord = action.payload.totalRecords;
       }
     });
 
-    //@CaseNo       04
-    //@Request      POST
-    //@Status       Success
-    //@Loading      False
-    //@used for     Add Customer
-    //@Response     Success Alert
+    // =============================================
+    // GET SINGLE CUSTOMER HANDLER
+    // =============================================
+    builder.addCase(getSingleCustomer.fulfilled, (state, action) => {
+      if (action.payload.success === true) {
+        state.current = action.payload.data;
+      }
+    });
+
+    // =============================================
+    // ADD CUSTOMER HANDLER
+    // =============================================
     builder.addCase(addCustomer.fulfilled, (state, action) => {
-      console.log(action.payload);
-      //Check for errors
+      // Handle errors
       if (action.payload?.errors?.length > 0) {
-        return {
-          ...state,
-          errors: action.payload.errors,
-        };
+        state.errors = action.payload.errors;
+        return;
       }
 
-      
-      //Check for success status
+      // Handle success
       if (action.payload?.success === true) {
-        //toast
         toast(action.payload.msg, { position: "top-right", type: "success" });
-      
-        //Modifying id
+        
         const customer = {
           ...action.payload.customer,
           id: action.payload.customer._id,
         };
 
-        //If items are 5 then remove one item from screen and add ne item
+        // Maintain maximum 5 items in the list
         if (state.data.length === 5) {
-          let popedState = []; // Creating empty array
-          //Inserting all items except last one
-          state.data.forEach(
-            (item, index) => index < 4 && popedState.push(item)
-          );
-          return {
-            ...state,
-            data: [customer, ...popedState],
-            errors: [],
-            totalRecord: action.payload.totalRecord
-          };
+          const poppedState = state.data.slice(0, 4);
+          state.data = [customer, ...poppedState];
         } else {
-          return {
-            ...state,
-            data: [customer, ...state.data],
-            errors: [],
-            totalRecord: action.payload.totalRecord
-          };
+          state.data = [customer, ...state.data];
         }
+        
+        state.totalRecord = action.payload.totalRecord;
+        state.errors = [];
       }
-
     });
 
-    //@CaseNo       05
-    //@Request      PUT
-    //@Status       Success
-    //@Loading      False
-    //@used For     Update Customer
-    //@Response     Success Alert
+    // =============================================
+    // UPDATE CUSTOMER HANDLER
+    // =============================================
     builder.addCase(updateCustomer.fulfilled, (state, action) => {
+      // Handle errors
       if (action.payload?.errors?.length > 0) {
-        return {
-          ...state,
-          errors: action.payload.errors,
-        };
+        state.errors = action.payload.errors;
+        return;
       }
-      //Check for Success
+
+      // Handle success
       if (action.payload.success === true) {
+        toast(action.payload.msg, { position: "top-right", type: "success" });
+        
         const updatedCustomer = {
           ...action.payload.updated,
           id: action.payload.updated._id,
         };
-        //Show Alert
-        toast(action.payload.msg, { position: "top-right", type: "success" });
-        return {
-          ...state,
-          data: state.data.map((item) =>
-            item.id === action.payload.updated._id ? updatedCustomer : item
-          ),
-          errors: [],
-        };
+
+        // Update customer in array
+        state.data = state.data.map(item =>
+          item.id === updatedCustomer.id ? updatedCustomer : item
+        );
+        
+        state.errors = [];
       }
     });
-    //@CaseNo       05
-    //@Request      DELETE
-    //@Status       Success
-    //@Loading      False
-    //@used for     DELETE CUSTOMER
-    //@Data         Filtered data stored
+
+    // =============================================
+    // DELETE CUSTOMER HANDLER
+    // =============================================
     builder.addCase(deleteCustomer.fulfilled, (state, action) => {
-      //Check for request success
       if (action.payload.success === true) {
-        //Set Alert
-        toast(action.payload.msg, { position: "bottom-right", type: "error" });
-        return {
-          ...state,
-          data: [...state.data.filter((item) => item.id !== action.payload.id)],
-          totalRecord: action.payload.totalRecords,
-        };
+        toast(action.payload.msg, { position: "top-right", type: "error" });
+        
+        // Remove deleted customer from array
+        state.data = state.data.filter(item => item.id !== action.payload.id);
+        state.totalRecord = action.payload.totalRecords;
       }
     });
   },
 });
 
-//Export Reducer functions
+// Export actions and reducer
 export const { clearCustomers } = customerSlice.actions;
 export default customerSlice.reducer;
